9e84a66d0db7d66ba16a978bcf6cfe384c7d6277
pauloaula <paulo.abd3@hotmail.com>
2024-12-12 16:52:57 -0300

    Add files via upload

---
+++ aula/api/conec.php
@@ -0,0 +1,9 @@
+<?php 
+$localdb="mysql-dbproz.alwaysdata.net";
+$userdb="dbproz";
+$senhadb="pauloandre123";
+$nomedb="dbproz_aula";
+$port="3306";
+$conn = mysqli_connect($localdb,$userdb,$senhadb,$nomedb,$port);
+
+?>
